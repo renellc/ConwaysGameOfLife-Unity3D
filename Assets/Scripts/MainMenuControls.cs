cd7726103b3dd066fa18c1083a5281fa55796ba6
Renell Castro <renellc9819@gmail.com>
2018-12-19 19:00:23 -0600

    Added main menu controls

---
+++ Assets/Scripts/MainMenuControls.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControls : MonoBehaviour
+{
+    public void LoadBoardScene()
+    {
+        SceneManager.LoadScene("BoardScene");
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
