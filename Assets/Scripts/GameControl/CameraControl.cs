ecdb36d112c991fcceda5f71c4aa8d5714f1b167
Renell Castro <renellc9819@gmail.com>
2018-12-19 18:44:38 -0600

    Organized scripts, added namespaces

---
+++ Assets/Scripts/GameControl/CameraControl.cs
@@ -0,0 +1,99 @@
+﻿using ConwaysGameOfLife;
+using UnityEngine;
+
+namespace GameControl
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraControl : MonoBehaviour
+    {
+        /// <summary>
+        /// The main camera for the game.
+        /// </summary>
+        private Camera viewCamera;
+
+        /// <summary>
+        /// The zoom level that indicates the closest we can zoom in.
+        /// </summary>
+        [SerializeField]
+        private int minimumZoomLevel;
+
+        /// <summary>
+        /// The zoom level that indicates the farthest we can zoom out.
+        /// </summary>
+        [SerializeField]
+        private int maximumZoomLevel;
+
+        /// <summary>
+        /// How fast the camera moves when being panned.
+        /// </summary>
+        [SerializeField]
+        private float panSpeed;
+
+        /// <summary>
+        /// The reference to the board.
+        /// </summary>
+        [SerializeField]
+        private GameOfLife board;
+
+        private void Start()
+        {
+            viewCamera = Camera.main;
+            var tilemapCenter = board.tilemap.cellBounds.center;
+            tilemapCenter.z = viewCamera.transform.position.z;
+            viewCamera.transform.position = tilemapCenter;
+        }
+
+        private void Update()
+        {
+            MoveCamera();
+            ZoomCamera();
+        }
+
+        /// <summary>
+        /// Moves the camera in some direction (based on user input).
+        /// </summary>
+        private void MoveCamera()
+        {
+            // Get the user's input.
+            var moveDirection = new Vector3
+            {
+                x = Input.GetAxisRaw("Horizontal"),
+                y = Input.GetAxisRaw("Vertical")
+            };
+
+            // We normalize the user input to get the true direction (where the magnitude of the vector
+            // is equal to 1).
+            moveDirection.Normalize();
+
+            // Scale the move direction by the pan speed.
+            moveDirection *= panSpeed * Time.deltaTime;
+
+            // Update the camera's position.
+            var targetPos = viewCamera.transform.position + moveDirection;
+            viewCamera.transform.position = targetPos;
+        }
+
+        /// <summary>
+        /// Adjust's the camera's zoom on the board.
+        /// </summary>
+        private void ZoomCamera()
+        {
+            // Zoom out
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                viewCamera.orthographicSize--;
+
+            }
+
+            // Zoom in
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                viewCamera.orthographicSize++;
+            }
+
+            // Restrict the camera's zoom by the values set in the inspector.
+            viewCamera.orthographicSize = Mathf.Clamp(viewCamera.orthographicSize, minimumZoomLevel,
+                maximumZoomLevel);
+        }
+    }
+}
---
+++ Assets/Scripts/GameOfLife/GameOfLife.cs
@@ -0,0 +1,232 @@
+﻿using GameControl;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+namespace ConwaysGameOfLife
+{
+    [RequireComponent(typeof(Tilemap), typeof(MouseControl))]
+    public class GameOfLife : MonoBehaviour
+    {
+        #region Tiles
+
+        [Header("Board Tiles")]
+        /// <summary>
+        /// The tile used for when a cell is alive.
+        /// </summary>
+        public Tile aliveTile;
+
+        /// <summary>
+        /// The tile used for when a cell is dead.
+        /// </summary>
+        public Tile deadTile;
+
+        #endregion
+
+        #region Board settings
+
+        [Header("Board Settings")]
+        /// <summary>
+        /// The width of the board.
+        /// </summary>
+        public int width = 48;
+
+        /// <summary>
+        /// The height of the board.
+        /// </summary>
+        public int height = 27;
+
+        /// <summary>
+        /// The default speed at which the simulation will run at.
+        /// </summary>
+        [SerializeField, Range(0, 0.9f)]
+        private float defaultSimulationSpeed = 0;
+
+        #endregion
+
+        #region Components
+
+        [Header("Components")]
+        /// <summary>
+        /// The tilemap that contains the cells of our grid.
+        /// </summary>
+        [HideInInspector]
+        public Tilemap tilemap;
+
+        /// <summary>
+        /// The component for the controlling the mouse.
+        /// </summary>
+        private MouseControl mouseControl;
+
+        /// <summary>
+        /// The UI slider component for adjusting the simulation speed.
+        /// </summary>
+        [SerializeField]
+        private Slider simSpeedSlider;
+
+        #endregion
+
+        #region Class members
+
+        /// <summary>
+        /// The current simulation speed.
+        /// </summary>
+        private float simulationSpeed;
+
+        /// <summary>
+        /// Contains the states of each cell in the grid. If a cell (x, y) is true, the cell is
+        /// considered to be alive, otherwise it is dead.
+        /// </summary>
+        [HideInInspector]
+        public Cell[,] gridState;
+
+        /// <summary>
+        /// Is the simulation currently running?
+        /// </summary>
+        [HideInInspector]
+        public bool isRunning;
+
+        #endregion
+
+        private void Start()
+        {
+            tilemap = GetComponent<Tilemap>();
+            mouseControl = GetComponent<MouseControl>();
+
+            simulationSpeed = defaultSimulationSpeed;
+            simSpeedSlider.value = 1 - simulationSpeed;
+
+            isRunning = false;
+            gridState = new Cell[width, height];
+
+            // All cells start off dead
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    gridState[x, y] = new Cell(x, y, false);
+                    tilemap.SetTile(new Vector3Int(x, y, 0), deadTile);
+                }
+            }
+
+            tilemap.ResizeBounds();
+        }
+
+        /// <summary>
+        /// Starts the game of life simulation. This is called when the 'Start' button is pressed.
+        /// </summary>
+        public void StartGameOfLifeSimulation()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                mouseControl.allowedToEdit = false;
+                StartCoroutine(StartSimulation());
+            }
+        }
+
+        /// <summary>
+        /// Stops the Game of Life simulation. This is called when the 'Stop' button is pressed.
+        /// </summary>
+        public void StopSimulation()
+        {
+            isRunning = false;
+            mouseControl.allowedToEdit = true;
+        }
+
+        /// <summary>
+        /// Stops the current running simulation and resets the board. This is called whe nthe 'Reset'
+        /// button is pressed.
+        /// </summary>
+        public void ResetBoard()
+        {
+            isRunning = false;
+            mouseControl.allowedToEdit = true;
+
+            // Set all cells as dead.
+            for (int x = 0; x < gridState.GetLength(0); x++)
+            {
+                for (int y = 0; y < gridState.GetLength(1); y++)
+                {
+                    gridState[x, y].Alive = false;
+                    tilemap.SetTile(new Vector3Int(x, y, 0), deadTile);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts the Game Of Life simulation.
+        /// </summary>
+        private IEnumerator StartSimulation()
+        {
+            while (isRunning)
+            {
+                // Create a new grid state.
+                Cell[,] nextGridState = new Cell[width, height];
+
+                // Update each cell's state and place them in a new grid state.
+                for (int x = 0; x < gridState.GetLength(0); x++)
+                {
+                    for (int y = 0; y < gridState.GetLength(1); y++)
+                    {
+                        nextGridState[x, y] = UpdateCellState(gridState, gridState[x, y]);
+                    }
+                }
+
+                // Update our current grid state.
+                gridState = nextGridState;
+
+                // Render the new grid state.
+                for (int x = 0; x < gridState.GetLength(0); x++)
+                {
+                    for (int y = 0; y < gridState.GetLength(1); y++)
+                    {
+                        if (gridState[x, y].Alive)
+                        {
+                            tilemap.SetTile(new Vector3Int(x, y, 0), aliveTile);
+                        }
+                        else
+                        {
+                            tilemap.SetTile(new Vector3Int(x, y, 0), deadTile);
+                        }
+                    }
+                }
+
+                // Wait a set amount of seconds before running this coroutine again.
+                yield return new WaitForSeconds(simulationSpeed);
+            }
+        }
+
+        /// <summary>
+        /// Updates a cell's state in the grid.
+        /// </summary>
+        /// <param name="grid">The current state of the grid.</param>
+        /// <param name="cell">The cell to update.</param>
+        /// <returns>The updated state of the cell.</returns>
+        private Cell UpdateCellState(Cell[,] grid, Cell cell)
+        {
+            Cell newCellState = new Cell(cell.X, cell.Y, false);
+            int livingCellCount = cell.LivingNeighborCount(grid);
+
+            if (cell.Alive)
+            {
+                newCellState.Alive = livingCellCount > 1 && livingCellCount < 4;
+            }
+            else
+            {
+                newCellState.Alive = livingCellCount == 3;
+            }
+
+            return newCellState;
+        }
+
+        /// <summary>
+        /// Adjusts the speed at which the simulation runs at.
+        /// </summary>
+        public void AdjustSimulationSpeed()
+        {
+            simulationSpeed = 1 - simSpeedSlider.value;
+        }
+    }
+}
